2636e9e00f697bbfdc3b7a8da8ea4a7778113f59
eminence <umwanamwiza8@gmail.com>
2024-10-17 14:57:33 +0200

    v12.8

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,34 @@
-import {headerLogo} from '../assets/images'
-import { navLinks } from '../constants'
-import Button from './Button'
-import { Link } from 'react-scroll'
-import MenuBar from './MenuBar'
+import { Link } from 'react-router-dom';
+import { headerLogo } from '../assets/images';
+import { navLinks } from '../constants';
+import Button from './Button';
+import MenuBar from './MenuBar';
 
 const Navbar = () => {
   return (
     <nav className='nav max-lg:px-8'>
-          <a href="/">
-                <img src={headerLogo} alt="Nike" 
-                width={130} height={29} />
-          </a>
-          <ul className='nav-list'>
-            {navLinks.map((item=>(
-              <li key={item.label} >
-                <a href={item.href} className='nav-li text-xl'>{item.label}</a>
-              </li>
-            )))}
-            <li className='right-12 absolute'><Button label="Shop Now" link='https://igitiecom.netlify.app/'/></li>
-          </ul>
-        
-          <div className='hidden max-lg:block'>
-          <MenuBar/>
-          </div>
-    </nav>
-  )
-}
+      <a href="/">
+        <img src={headerLogo} alt="Nike" width={130} height={29} />
+      </a>
+      <ul className='nav-list'>
+        {navLinks.map((item) => (
+          <li key={item.label}>
+            {/* Use Link from react-router-dom for dynamic navigation */}
+            <Link to={item.href} className='nav-li text-xl'>
+              {item.label}
+            </Link>
+          </li>
+        ))}
+        <li className='right-12 absolute'>
+          <Button label="Shop Now" link='https://igitiecom.netlify.app/' />
+        </li>
+      </ul>
 
-export default Navbar
+      <div className='hidden max-lg:block'>
+        <MenuBar />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,13 @@ import Navbar from './components/Navbar';
 import Footer from './sections/Footer';
 import Faq from './pages/Faq';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Navbar />
-      <main className="container mx-auto">
+      <main className="">
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/contact" element={<Contact />} />
