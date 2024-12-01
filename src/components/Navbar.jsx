8aad2f4402735024ebe3129e092beb82c603c248
eminence <umwanamwiza8@gmail.com>
2024-12-02 00:18:54 +0200

    cool hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,29 +7,39 @@ import MenuBar from './MenuBar';
 
 const Navbar = () => {
   const { navLinks, store } = useData(); // Access navLinks from global data context
-  // console.log(navLinks)
   return (
-    <nav className='nav max-lg:px-8'>
-      <a href="/">
-        <img src={headerLogo} alt="Logo" width={130} height={29} />
-      </a>
-      <ul className='nav-list'>
-        {navLinks?.map((item) => (
-          <li key={item.href}>
-            <Link to={item.href} className='nav-li text-xl'>
-              {item.label}
-            </Link>
-          </li>
-        ))}
-        <li className='right-12 absolute'>
-          <Button label={store.label} link={'https://buy.igiti.africa' || store.link} />
-        </li>
-      </ul>
+    <div className="relative">
+      <div className="absolute z-20 inset-x-0 top-4 flex justify-center">
+      <div className="bg-slate-100 opacity-85 rounded-2xl shadow-lg px-4 max-w-4xl flex items-center justify-between">
+        <nav className="flex items-center justify-between px-4 -my-2 gap-12">
+          {/* Logo */}
+          <a href="/" className="">
+            <img src={headerLogo} alt="Logo" width={125} height={25} />
+          </a>
 
-      <div className='hidden max-lg:block'>
-        <MenuBar />
+          {/* Navigation Links */}
+          <ul className="flex items-center space-x-12">
+            {navLinks?.map((item) => (
+              <li key={item.href}>
+                <Link to={item.href} className="nav-li text-xl text-gray-700 hover:text-gray-900">
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          {/* Button */}
+          <Button label={store.label} link={'https://buy.igiti.africa' || store.link} />
+
+          {/* Mobile Menu */}
+          <div className="lg:hidden">
+            <MenuBar />
+          </div>
+        </nav>
       </div>
-    </nav>
+    </div>
+    </div>
+    
   );
 };
 
